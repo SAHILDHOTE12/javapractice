c1d413bcccc1d4828b14c2ae25cb446562f66650
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-03-09 20:20:08 +0530

    Create Ex11.java

---
+++ Ex11.java
@@ -0,0 +1,15 @@
+package implicit;
+
+public class EX11 {
+
+	public static void main(String[] args) {
+		{
+			float f=0.7f;
+			if(f==0.7);
+			System.out.println("Hello");
+			
+				System.out.println("Hi");
+			
+		}
+
+        	}
