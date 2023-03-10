f27bc94a1638bbbd70bab2f3cb5ff0f142e4ac83
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-03-10 11:57:14 +0530

    Create If.java

---
+++ If.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class IfPro{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter any Number: ");
+		int n=in.nextInt();
+			if (n>0) {
+				System.out.print(n+" is Positive");
+			}
+			if (n<0) {
+				System.out.print(n+" is Negative");
+			}
+			else{
+				System.out.print(n+" is Zero");
+			}
+	}
+}
