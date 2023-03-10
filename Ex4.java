fa8b9556ac621897e13b623c06c6b83ee484324a
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-03-10 21:01:22 +0530

    Create Ex4.java

---
+++ Ex4.java
@@ -0,0 +1,13 @@
+class Code4 {
+	
+	public static void main(String[] args) {
+		System.out.println(32);
+		System.out.println(4.5);
+		System.out.println("A");
+		System.out.println("##");
+		System.out.println("ABC");
+		
+		
+		}
+
+}
