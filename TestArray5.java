85e1c67f51c90385b3aa8ccd0eabe42e60784309
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-03-26 17:25:21 +0530

    Create TestArray5.java

---
+++ TestArray5.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class TestArray5{
+	public static void main(String[] args) {
+		System.out.println(new int[]{55,52,33,48,68,26}.length);
+		System.out.println(new int[]{91,44,17,41,11}[3]);
+	}
+}
