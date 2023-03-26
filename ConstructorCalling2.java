74296eb9a46d8364266ae997c162d65e613bee34
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-03-26 13:35:35 +0530

    Create ConstructorCalling2.java

---
+++ ConstructorCalling2.java
@@ -0,0 +1,24 @@
+class A{
+	public A(){
+		System.out.println("Class A Constructor");
+	}
+	public void showA(){
+		System.out.println("Class A Show");
+	}
+}
+class B extends A{
+	public B(){
+		System.out.println("Class B Constructor");
+	}
+	public void showB(){
+		System.out.println("Class B Show");
+	}
+}
+class ConstructorCalling2{
+	public static void main(String[] args) {
+		B obj=new B();
+		obj.showB();
+		A obj1=new A();
+		obj.showA();
+	}
+}
