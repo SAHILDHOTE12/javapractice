d5e4cbc8c505d28b063c98abb61d69a6b23ed921
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-03-27 09:13:16 +0530

    Create StringDemo10.java

---
+++ StringDemo10.java
@@ -0,0 +1,9 @@
+class StringDemo10{
+	public static void main(String[] args) {
+		String str1="AYUSH";
+		String str2=str1.toLowerCase();
+		System.out.println(str2);
+		str2=str1.toUpperCase();
+		System.out.println(str2);
+	}
+}
