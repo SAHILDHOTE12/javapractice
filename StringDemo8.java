05bd1dc969329c98d3a73f167dd26d0fad473e0d
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-03-27 09:11:40 +0530

    Create StringDemo8.java

---
+++ StringDemo8.java
@@ -0,0 +1,11 @@
+class StringDemo8{
+	public static void main(String[] args) {
+		String s1="Ayush";
+		boolean b=s1.equals("ayush");
+		System.out.println(b);
+		System.out.println(s1.equals("Ayush"));
+		String s2="Prashant";
+		boolean b1=s1.equals(s2);
+		System.out.println(b1);
+	}
+}
