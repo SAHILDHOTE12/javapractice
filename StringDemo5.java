a17d447b0e36fd0132741890c93bab759fa4fc09
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-03-27 09:09:10 +0530

    Create StringDemo5.java

---
+++ StringDemo5.java
@@ -0,0 +1,10 @@
+class StringDemo5{
+	public static void main(String[] args) {
+		String s="Babulal";
+		int i=s.indexOf('B');
+		int j=s.indexOf('l');
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(s.indexOf('p'));
+	}
+}
