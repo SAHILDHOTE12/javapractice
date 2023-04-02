afa589ed4bd244f723f71b08d19db52d39014d0e
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-04-02 19:16:59 +0530

    Create SingleInheritance2.java

---
+++ SingleInheritance2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Parent{
+	public void add(int a,int b){
+		System.out.println("The Addition is "+(a+b));
+	}
+}
+class Child1 extends Parent{
+	public void sub(int a,int b){
+		System.out.println("The Subtraction is "+(a-b));
+	}
+}
+class Child2 extends Child1{
+	public void mul(int a,int b){
+		System.out.println("The Multiplication is "+(a*b));
+	}
+}
+class Child3 extends Child2{
+	public void div(int a,int b){
+		System.out.println("The Division is "+(a/b));
+	}
+}
+class MultipleInheritance{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Two Numbers: ");
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	Child3 c=new Child3();
+	c.add(a,b);
+	c.sub(a,b);
+	c.mul(a,b);
+	c.div(a,b);
+	}
+}
