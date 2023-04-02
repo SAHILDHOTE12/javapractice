2bb6844e8ea5d798787bd841e37de26a8b90e7fe
Sahil Dhote <127387617+SAHILDHOTE12@users.noreply.github.com>
2023-04-02 19:24:29 +0530

    Create Interface1.java

---
+++ Interface1.java
@@ -0,0 +1,26 @@
+interface Abc{
+	void m1();
+	void m2();
+	void m3();
+}
+class Test implements Abc{
+	public void m1(){
+		System.out.println("m1 Method");
+	}
+	public void m2(){
+		System.out.println("m2 Method");
+	}
+	public void m3(){
+		System.out.println("m3 Method");
+	}
+}
+class Interface1{
+	public static void main(String[] args) {
+		Abc t=new Test();
+		t.m1();
+		t.m2();
+		t.m3();
+	}
+}
+Footer
+© 2023 GitHub, Inc.
